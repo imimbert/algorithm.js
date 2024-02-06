b99c713c3ceef85e2ace7da0c93d526277463a68
imimbert <2080035494@qq.com>
2024-02-06 20:19:11 +0800

    update index.js

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 const algorm = {};
-algorm.graph = require("./DataStructure/graph")
+algorm.graph = require("./DataStructure/graph");
+algorm.stack = require("./DataStructure/stack")
+algorm.queue = require("./DataStructure/queue")
+algorm.hash = require("./DataStructure/hash")
+algorm.tree = require("./DataStructure/tree")
+
+algorm.sort = require("./algorithm/sort")
 
 module.exports=algorm;
