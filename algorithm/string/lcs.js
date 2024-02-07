b99c63ac9c571a03e3d824e5ed8993ac62ba0857
imimbert <2080035494@qq.com>
2024-02-07 11:02:06 +0800

    update lcs.js

---
+++ algorithm/string/lcs.js
@@ -0,0 +1,37 @@
+/**
+ * longest common substring
+ * @param str1 string字符串 the string
+ * @param str2 string字符串 the string
+ * @return string字符串
+ */
+function lcs( str1 ,  str2 ) {
+    // write code here
+    let dp=[]
+    let maxi,maxj
+    let maxlen=0
+    for(let i=0;i<str1.length+1;i++){
+        dp[i]=[]
+        for(let j=0;j<str2.length+1;j++){
+            dp[i].push(0)
+        }
+    }
+    for(let i=1;i<str1.length+1;i++){
+        for(let j=1;j<str2.length+1;j++){
+            if(str1[i-1]!=str2[j-1]){
+                dp[i][j]=0
+            }else{
+                dp[i][j]=dp[i-1][j-1]+1;
+                if(dp[i][j]>maxlen){
+                    maxlen=dp[i][j]
+                    maxi=i
+                    maxj=j
+                }
+            }
+        }
+    }
+    return str2.slice(maxj-maxlen,maxj)
+    
+}
+
+console.log(LCS("mdrrrrr","idr"));
+module.exports = lcs;
